3f46aaf000e9895f042f65aa6f95551423ccf6ed
Clément Dubreuil <hello@clementd.fr>
2021-04-22 13:58:18 +0200

    s/update/cron/g

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ import Kernel from './daemon/kernel.js';
 import providers from './provider/index.js';
 import downloaders from './downloader/index.js';
 
-new Kernel(providers, downloaders).update();
+new Kernel(providers, downloaders).cron();
